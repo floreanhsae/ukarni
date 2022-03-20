a0b7306ebbf2d65b16f19a43e177ff9a117621e9
riski putra <subkhanpinter22@gmail.com>
2022-03-20 14:07:50 +0700

    add href

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,42 +3,42 @@
     <ul class="sidebar-nav" id="sidebar-nav">
 
       <li class="nav-item">
-        <a class="nav-link" href="index.html">
+        <a class="nav-link" href="/dashboard">
           <i class="bi bi-grid"></i>
           <span>Dashboard</span>
         </a>
       </li><!-- End Dashboard Nav -->
 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="pages-contact.html">
+        <a class="nav-link collapsed" href="/dashboard/info-lowongan">
           <i class="bi bi-briefcase-fill"></i>
           <span>Info Lowongan</span>
         </a>
       </li><!-- End Info Lowongan Page Nav -->
 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="pages-register.html">
+        <a class="nav-link collapsed" href="/dashboard/rekrutmen">
           <i class="bi bi-person-badge"></i>
           <span>Rekrutmen</span>
         </a>
       </li><!-- End Rekrutmen Page Nav -->
 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="pages-login.html">
+        <a class="nav-link collapsed" href="/dashboard/agenda">
           <i class="bi bi-calendar-check"></i>
           <span>Agenda</span>
         </a>
       </li><!-- End Agenda Page Nav -->
 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="pages-error-404.html">
+        <a class="nav-link collapsed" href="/dashboard/perusahaan">
           <i class="bi bi-building"></i>
           <span>Perusahaan</span>
         </a>
       </li><!-- End Perusahaan Page Nav -->
 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="pages-blank.html">
+        <a class="nav-link collapsed" href="/dashboard/user">
           <i class="bi bi-person-fill"></i>
           <span>User</span>
         </a>
@@ -46,7 +46,7 @@
       <!-- End User Page Nav -->
 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="pages-blank.html">
+        <a class="nav-link collapsed" href="/dashboard/other">
           <i class="bi bi-three-dots-vertical"></i>
           <span>Other</span>
         </a>

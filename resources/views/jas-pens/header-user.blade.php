5c3711787115721acb91a910a16da80b97eb7e93
Waridun Nafis <waridunnafis@gmail.com>
2022-03-22 20:16:01 +0700

    Vertical line update

--- resources/views/jas-pens/header-user.blade.php
+++ resources/views/jas-pens/header-user.blade.php
@@ -13,6 +13,7 @@
             <li class="nav-item">
                 <a class="nav-link px-3" href="#">Akreditasi</a>
             </li>
+
             <li class="nav-item dropdown text-center px-3">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
                     data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">

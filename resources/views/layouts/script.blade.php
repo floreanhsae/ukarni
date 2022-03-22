13e9ed8eb2be286de32ba0bdf1f20e39de47d7a9
Waridun Nafis <waridunnafis@gmail.com>
2022-03-22 20:51:18 +0700

    dropdown

--- resources/views/layouts/script.blade.php
+++ resources/views/layouts/script.blade.php
@@ -1,7 +1,9 @@
 <!-- Vendor JS Files -->
 <script src="../vendor/apexcharts/apexcharts.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js" integrity="sha512-k2WPPrSgRFI6cTaHHhJdc8kAXaRM4JBFEDo1pPGGlYiOyv4vnA0Pp0G5XMYYxgAPmtmv/IIaQA6n5fLAyJaFMA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+{{-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script> --}}
 <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="../vendor/chart.js/chart.min.js"></script>
+<script src="../vendor/chart.js/chart.min.js"></scri>
 <script src="../vendor/echarts/echarts.min.js"></script>
 <script src="../vendor/quill/quill.min.js"></script>
 <script src="../vendor/simple-datatables/simple-datatables.js"></script>

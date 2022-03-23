be70da66f2a94c1147e317f0dc81c46a665a133c
brodinstylelistic <syarifudinmuhammad67@gmail.com>
2022-03-23 10:45:58 +0700

    tampilan edit sudah aman

--- resources/views/layouts/style.blade.php
+++ resources/views/layouts/style.blade.php
@@ -7,13 +7,13 @@
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
 <!-- Vendor CSS Files -->
-<link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-<link href="../vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-<link href="../vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-<link href="../vendor/quill/quill.snow.css" rel="stylesheet">
-<link href="../vendor/quill/quill.bubble.css" rel="stylesheet">
-<link href="../vendor/remixicon/remixicon.css" rel="stylesheet">
-<link href="../vendor/simple-datatables/style.css" rel="stylesheet">
+<link href="{{asset('css/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+<link href="{{asset('css/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+<link href="{{asset('css/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
+<link href="{{asset('css/quill/quill.snow.css')}}" rel="stylesheet">
+<link href="{{asset('css/quill/quill.bubble.css')}}" rel="stylesheet">
+<link href="{{asset('css/remixicon/remixicon.css')}}" rel="stylesheet">
+<link href="{{asset('css/simple-datatables/style.css')}}" rel="stylesheet">
 
 <!-- Template Main CSS File -->
 <link rel="stylesheet" href="{{asset('css/style.css')}}">

b7c0420774c92ba3ddbf152caaa17460a784b0ef
brodinstylelistic <syarifudinmuhammad67@gmail.com>
2022-03-24 20:47:31 +0700

    tampilan user 97%

--- resources/views/jas-pens/header-user.blade.php
+++ resources/views/jas-pens/header-user.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark fixed-top py-3">
     <div class="container">
-        <img class="navbar-brand mr-1" src="{{ url('/img/logojas.png') }}" href="#">
+        <a href=https://www.pens.ac.id/"><img class="navbar-brand mr-1" src="{{ url('/img/logojas.png') }}" href="https://www.pens.ac.id/"></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
